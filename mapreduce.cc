7632865350db5c71905519dded24a431c7928ad5
rqberry <qberry.ryan@gmail.com>
2020-10-23 10:48:39 -0700

    Don't need NUM_THREADS

--- mapreduce.cc
+++ mapreduce.cc
@@ -3,11 +3,6 @@
 #include <sstream>
 #include <fstream>
 
-int NUM_THREADS = 1; // single thread
-/*
-int NUM_THREADS = get_nprocs_conf(); // multi-thread
-*/
-
 void MapReduce::MR_Emit(const std::string& key, const std::string& value) {
 
     return;

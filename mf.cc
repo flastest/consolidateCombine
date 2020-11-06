2342d75ccacb6f6ba1a04fa2c45bd3184d134a7e
flastest <pleasantbowel5yndrome@gmail.com>
2020-11-06 10:36:51 -0800

    mf works

--- mf.cc
+++ mf.cc
@@ -4,11 +4,21 @@
 #include <string>
 #include <iostream>     //for reading from file
 #include <fstream>      //also reading from a file
-//#include "mapreduce.cc"
+#include "mapreduce.cc"
 #include <unordered_map>
 #include <mutex>
 #include <string.h>
 #include <iterator>  //for std::size
+
+using mf_t = std::unordered_map<std::string,std::string>; //shard of kv pairs
+
+mf_t mutual_friends;
+std::mutex mf_mutex;
+//entry will be like RyanAriel:Eitan,Jim
+
+
+
+int num_friends; 
 
 void Map(const char* file_name) {
     FILE *fp = fopen(file_name, "r");
@@ -27,9 +37,8 @@ void Map(const char* file_name) {
         }*/
         ///*
         char* next;
-        while (token != NULL) {
+        while (token != NULL && *token != '\r' && *token != '\n') {
             std::string s(token);
-            //MapReduce::MR_Emit(token, root);
             next = strtok(NULL,",");
             //trim the newline off of s
             if (!next) 
@@ -38,6 +47,8 @@ void Map(const char* file_name) {
                 s = s.substr(0,s.find('\n'));
                 s = s.substr(0,s.find('\r'));
             }
+            MapReduce::MR_Emit(s, root_friend);
+            
             //std::cout <<"friend is still" <<s<<std::endl;
             //std::cout <<"printing s again"<<s<<std::endl;
             std::cout <<s<<":"<<root_friend<<std::endl;
@@ -47,28 +58,38 @@ void Map(const char* file_name) {
     free(line);
     fclose(fp);
 }
-/*
+
+
 void Reduce(std::string key, MapReduce::getter_t get_next, int partition_number) {
     //std::cout << "wc:Reduce() Begin" << std::endl;
     int count = 0;
+    std::vector<std::string> list_of_friends;
+    std::string friend_name; //this is the name of all the people with the same mutual friend
     std::string val = get_next(key, partition_number);
     while (!val.empty()) {
-        count++;
+        friend_name.append(val);
         val = get_next(key, partition_number);
     }
-    std::lock_guard<std::mutex> guard(count_mutex);
-    counts[key] = count;
+    std::lock_guard<std::mutex> guard(mf_mutex);
+    if (mutual_friends[friend_name].empty())
+
+        mutual_friends[friend_name] = key;
+    else {
+        mutual_friends[friend_name].append(", ");
+        mutual_friends[friend_name].append(key);
+    }
     //std::cout << key << count << std::endl;
 }
-*/
+
 int main(int argc, char *argv[]) {
-    /*
+    num_friends = argc-1;
+    
     MapReduce::MR_Run(argc, argv, Map, 4, Reduce, 4, MapReduce::MR_DefaultHashPartition);
-    for(auto kv : counts) {
+    for(auto kv : mutual_friends) {
         std::cout << "[" <<kv.first <<"] : " <<kv.second <<", "<<std::flush;
     }
     std::cout<<std::endl;
-    */
+    
     Map(argv[1]);
     return 0;
 }

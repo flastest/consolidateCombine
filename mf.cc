7b118dac339d2006b16538f0693baf34405ce2cc
flastest <pleasantbowel5yndrome@gmail.com>
2020-11-06 09:25:02 -0800

    removed test files from big directory and fixed mf

--- mf.cc
+++ mf.cc
@@ -8,6 +8,7 @@
 #include <unordered_map>
 #include <mutex>
 #include <string.h>
+#include <iterator>  //for std::size
 
 void Map(const char* file_name) {
     FILE *fp = fopen(file_name, "r");
@@ -17,12 +18,31 @@ void Map(const char* file_name) {
     while (getline(&line, &size, fp) != -1) {
         char *token = line;
         char *root_friend = strtok(line,";"); //Get the root friend.
-	token = strtok(NULL,",");
-        while (token != NULL) {
+        token = strtok(NULL,",");
+        /*char *token, *dummy = root_friend;
+        while ((token = strsep(&dummy, " ")) != NULL) {
             std::cout<<token<<":"<<root_friend<<std::endl;
+            //MapReduce::MR_Emit(token, "1");
+            //dummy = token;
+        }*/
+        ///*
+        char* next;
+        while (token != NULL) {
+            std::string s(token);
             //MapReduce::MR_Emit(token, root);
-	    token = strtok(NULL,",");
-        }
+            next = strtok(NULL,",");
+            //trim the newline off of s
+            if (!next) 
+            {
+                //std::cout<<"length of "<<s<< " is "<<static_cast<int>(s.length())<<std::endl;
+                s = s.substr(0,s.find('\n'));
+                s = s.substr(0,s.find('\r'));
+            }
+            //std::cout <<"friend is still" <<s<<std::endl;
+            //std::cout <<"printing s again"<<s<<std::endl;
+            std::cout <<s<<":"<<root_friend<<std::endl;
+            token = next;
+        }//*/
     }
     free(line);
     fclose(fp);
